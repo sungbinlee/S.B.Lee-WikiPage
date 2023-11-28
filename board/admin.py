a627335d9d42903f19922af49bd388c0af6b9bc5
sungbinlee <sunglee@addu.edu.ph>
2023-11-28 12:38:01 +0900

    feat: Register Post and RelatedPost model with custom list_display

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, RelatedPost
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "content"]
+
+
+@admin.register(RelatedPost)
+class RelatedPost(admin.ModelAdmin):
+    list_display = ["id", "post", "related_to", "similarity"]
+
--- codinghub/urls.py
+++ codinghub/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("board/", include("board.urls")),
 ]
 
 if settings.DEBUG:
