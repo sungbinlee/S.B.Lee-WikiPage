ee5129bfb4bf7df4eed85611cd7a04d7e3eaf91b
sungbinlee <sunglee@addu.edu.ph>
2023-11-29 20:02:33 +0900

    feat: crate PostListView, PostDetailView and set urls

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+from board import views
 
+app_name = "board"
 
 urlpatterns =[
-
+    path("", views.post_list, name="post_list"),
+    path("<int:pk>/", views.post_detail, name="post_detail"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from board.models import Post
 
-# Create your views here.
+
+class PostListview(ListView):
+    model = Post
+    paginate_by = 15
+
+
+post_list = PostListview.as_view()
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs
+
+
+post_detail = PostDetailView.as_view()
+
+
+# def post_detail(request, pk):
+#     related_posts = [1,2,3,4]
+#     context = {
+#         "related_posts": related_posts,
+#     }
+#     return render(request, "board/post_detail.html", context)
