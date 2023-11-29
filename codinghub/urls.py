4284c95da1a4091467436eda6361500eac07d7a2
sungbinlee <sunglee@addu.edu.ph>
2023-11-29 20:05:47 +0900

    feat: Add root url redirection to board:post_list

--- codinghub/urls.py
+++ codinghub/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("board/", include("board.urls")),
+    path("", RedirectView.as_view(pattern_name="board:post_list"), name="root"),
 ]
 
 if settings.DEBUG:
