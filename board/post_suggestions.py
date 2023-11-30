4d291c29e1e3e0272ca21cf3452d9d18f0af8cbf
sungbinlee <sunglee@addu.edu.ph>
2023-11-30 23:09:39 +0900

    feat: add Korean stopwords for preprocessing

--- board/post_suggestions.py
+++ board/post_suggestions.py
@@ -17,7 +17,7 @@ with open(file_path, 'r', encoding='utf-8') as f:
 stop_words = file_content.split('\n')
 
 # 불용어 제거(조사, 관사, 동사 etc.)
-all_contents["content"] = all_contents["content"].apply(lambda x: re.sub(r'시켰습니다|으로써|적이며|라고|으로|합니다|입니다|에서|하고|하는|적인|로|을|를|은|는|이|가|한|에|과|의|와|들|$', '', x))
+all_contents["content"] = all_contents["content"].apply(lambda x: re.sub(r'시켰습니다|으로써|적이며|라고|으로|합니다|입니다|에서|롭게|하면|하고|하는|적인|시킬|로|할|을|를|은|는|이|가|한|에|과|의|와|들|$', '', x))
 
 # TF-IDF 벡터화
 tfidf_vectorizer = TfidfVectorizer(stop_words=stop_words, max_df=0.6, min_df=2)  # 전체 문서 에서 60% 이상 나온 단어 무시
