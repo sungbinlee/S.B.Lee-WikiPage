0868dda48bceb786fbfaa559be7e8e6211ad9a76
sungbinlee <sunglee@addu.edu.ph>
2023-12-01 20:35:47 +0900

    remove: RelatedPost model

--- board/models.py
+++ board/models.py
@@ -16,11 +16,3 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-id']
-
-
-class RelatedPost(models.Model):
-    #  * 게시글은 연관 게시글이라는 항목을 가지고 있으며, 연관게시글은 게시글과 내용이 유사한 게시글 입니다.
-    #  * 연관게시글이 보여지는 순서는 연관도가 높은것을 우선적으로 보여주도록 만들어주시면 더 좋습니다.
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="related_post")
-    related_to = models.ForeignKey(Post, on_delete=models.CASCADE)
-    similarity = models.FloatField(default=0.0)
