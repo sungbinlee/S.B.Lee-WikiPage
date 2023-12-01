19e46ce186e0470e404886ff6685b9f6b26eb2cd
sungbinlee <sunglee@addu.edu.ph>
2023-12-01 20:39:20 +0900

    remove: Unregister deleted model and remigrations

--- board/admin.py
+++ board/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import Post, RelatedPost
+from .models import Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "content"]
     list_display_links = ['title']
-
-
-@admin.register(RelatedPost)
-class RelatedPost(admin.ModelAdmin):
-    list_display = ["id", "post", "related_to", "similarity"]
-
